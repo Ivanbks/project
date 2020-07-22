48adb1bc4bedeeb64fbf279048f37057bc442960
ivan <b.ivan.kstovo@gmail.com>
2020-07-22 18:35:07 +0300

    комментарий чтобы понять что это за файл

---
+++ script.js
@@ -0,0 +1,16 @@
+"use strict"
+//alert("hello word");
+
+//const result = confirm('are yoe here?');
+//console.log(result);
+
+//const answer = +prompt('Вам есть 18?', 'yes');
+//console.log(typeof(answer));
+
+const answer = [];
+
+answer[0] = prompt('как ваше имя?',  '');
+answer[1] = prompt('как ваша фамилия?',  '');
+answer[2] = prompt('как ваша национальность',  '');
+
+document.write(answer);
